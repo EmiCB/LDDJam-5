b255d67619bcf0bc041f76bfac5c8b58a42bc96f
emicb <emicb55@gmail.com>
2021-01-02 16:01:51 -0600

    vertical camera

--- LDD-5/Assets/Scripts/CameraController.cs
+++ LDD-5/Assets/Scripts/CameraController.cs
@@ -6,6 +6,8 @@ public class CameraController : MonoBehaviour {
     private PlayerController player;
     private Vector3 lastPlayerPosition;
     private float distanceToMoveX;
+    private float distanceToMoveY;
+    [SerializeField] private float yThreshold = 3.0f;
 
     private void Start() {
         player = FindObjectOfType<PlayerController>();
@@ -14,7 +16,8 @@ public class CameraController : MonoBehaviour {
 
     private void Update() {
         distanceToMoveX = player.transform.position.x - lastPlayerPosition.x;
-        transform.position = new Vector3(transform.position.x + distanceToMoveX, transform.position.y, transform.position.z);
+        distanceToMoveY = Mathf.Abs(player.transform.position.y - this.transform.position.y) > yThreshold ? player.transform.position.y - lastPlayerPosition.y : 0;
+        transform.position = new Vector3(transform.position.x + distanceToMoveX, transform.position.y + distanceToMoveY, transform.position.z);
         lastPlayerPosition = player.transform.position;
     }
 }

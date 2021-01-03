e1ef31a04dea585f9c8ea75b32d8055b3fbfaa77
Robert Boyce <robert@rboyce.com>
2021-01-03 16:22:59 -0600

    update ability colors from new sprites

--- LDD-5/Assets/Scripts/ColorPicker.cs
+++ LDD-5/Assets/Scripts/ColorPicker.cs
@@ -5,23 +5,23 @@ using UnityEngine;
 public class ColorPicker {
     public static Color HeadAbilityColorPicker(PlayerController.HeadAbility headAbility) {
         return headAbility switch {
-            PlayerController.HeadAbility.None => Color.gray,
-            PlayerController.HeadAbility.MovementSpeedUp => Color.yellow
+            PlayerController.HeadAbility.None => new Color(155.0f / 255.0f, 173.0f / 255.0f, 183.0f / 255.0f, 1.0f),
+            PlayerController.HeadAbility.MovementSpeedUp => new Color(95.0f / 255.0f, 205.0f / 255.0f, 228.0f / 255.0f, 1.0f),
         };
     }
 
     public static Color ArmAbilityColorPicker(PlayerController.ArmAbility armAbility) {
         return armAbility switch {
-            PlayerController.ArmAbility.None => Color.gray,
-            PlayerController.ArmAbility.WallCling => Color.red
+            PlayerController.ArmAbility.None => new Color(155.0f / 255.0f, 173.0f / 255.0f, 183.0f / 255.0f, 1.0f),
+            PlayerController.ArmAbility.WallCling => new Color(143.0f / 255.0f, 86.0f / 255.0f, 59.0f / 255.0f, 1.0f),
         };
     }
 
     public static Color LegAbilityColorPicker(PlayerController.LegAbility legAbility) {
         return legAbility switch {
-            PlayerController.LegAbility.None => Color.gray,
-            PlayerController.LegAbility.DoubleJump => Color.green,
-            PlayerController.LegAbility.FastDrop => Color.cyan
+            PlayerController.LegAbility.None => new Color(155.0f / 255.0f, 173.0f / 255.0f, 183.0f / 255.0f, 1.0f),
+            PlayerController.LegAbility.DoubleJump => new Color(153.0f / 255.0f, 229.0f / 255.0f, 80.0f / 255.0f, 1.0f),
+            PlayerController.LegAbility.FastDrop => new Color(217.0f / 255.0f, 87.0f / 255.0f, 99.0f / 255.0f, 1.0f),
         };
     }
 }
